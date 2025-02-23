8bad9ded606b61cfbeaa2b72918db67431824dd3
Judah Fuller <judahfuller@icloud.com>
2025-02-23 16:58:38 +0000

    feat: Grab the current slide

--- src/actions.ts
+++ src/actions.ts
@@ -17,7 +17,7 @@ export function UpdateActions(self: ModuleInstance): void {
 					type: 'number',
 					label: 'Maxium number of slides',
 					default: 1,
-					min: 8,
+					min: 1,
 					max: self.config.num_slides,
 				},
 				{
@@ -61,7 +61,7 @@ export function UpdateActions(self: ModuleInstance): void {
 				}
 				self.log('debug', 'group found')
 
-				const selected_slides = selected_group.slides.slice(0, num - 1)
+				const selected_slides = selected_group.slides.slice(0, num)
 				if (selected_slides.length === 0) {
 					self.log('error', 'no slides found')
 					return
@@ -163,6 +163,93 @@ export function UpdateActions(self: ModuleInstance): void {
 				}
 
 				for (let i = 0; i < num; i++) {
+					const word = words[i]
+					self.log('debug', `Saving "${word}" to variable "word_${start + i}"`)
+					self.setVariableValues({
+						[`word_${start + i}`]: word,
+					})
+				}
+			},
+		},
+		grab_current_slide_action: {
+			name: 'Grab Current Slide',
+			options: [
+				{
+					id: 'num_words',
+					type: 'number',
+					label: 'Maxium number of words',
+					default: 32,
+					min: 1,
+					max: self.config.num_words,
+				},
+				{
+					id: 'start_words',
+					type: 'number',
+					label: 'Words variable start',
+					default: 1,
+					min: 1,
+					max: self.config.num_words,
+				},
+				{
+					id: 'text_id',
+					type: 'number',
+					label: 'Index to store word variable in',
+					default: 1,
+					min: 1,
+					max: self.config.num_slides,
+				},
+			],
+			callback: async (event) => {
+				const slide = await self.ProPresenter.statusSlide()
+				const num_words = event.options.num_words
+				const start = event.options.start_words
+				const text_id = event.options.text_id
+				if (slide === null) {
+					self.log('error', 'no presentation active')
+					return
+				}
+				if (typeof num_words !== 'number') {
+					self.log('error', 'num must be a number')
+					return
+				}
+				if (typeof start !== 'number') {
+					self.log('error', 'start must be a number')
+					return
+				}
+
+				self.log('debug', 'Slide active')
+				self.log('debug', `${JSON.stringify(slide)}`)
+
+				self.log('debug', `Clearing variable "slide_${text_id}"`)
+				self.setVariableValues({
+					[`slide_${text_id}`]: '',
+				})
+
+				self.log('debug', `Saving "${slide.data.current.text}" to variable "slide_${text_id}"`)
+				self.setVariableValues({
+					[`slide_${text_id}`]: slide.data.current.text,
+				})
+
+				const words: string[] = []
+				const text = slide.data.current.text.replaceAll('\n', ' ').split(' ')
+				for (let j = 0; j < text.length; j++) {
+					words.push(text[j])
+				}
+
+				if (words.length === 0) {
+					self.log('error', 'no words found')
+					return
+				}
+				self.log('debug', 'words found')
+
+				for (let i = 0; i < num_words; i++) {
+					self.log('debug', `Clearing variable "word_${start + i}"`)
+					self.setVariableValues({
+						[`word_${start + i}`]: '',
+					})
+				}
+
+				for (let i = 0; i < num_words; i++) {
 					const word = words[i]
 					self.log('debug', `Saving "${word}" to variable "word_${start + i}"`)
 					self.setVariableValues({
